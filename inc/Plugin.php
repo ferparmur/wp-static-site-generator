f87198219def571e544f82dcc395c451c06020b5
fernando-paredes <ferparmur@gmail.com>
2023-12-09 05:46:47 +0000

    initial commit

---
+++ inc/Plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ferparmur\WpStaticSiteGenerator;
+
+class Plugin {
+    const VERSION = '1.0.0';
+
+    public function __construct()
+    {
+
+    }
+}
---
+++ wp-static-site-generator.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Static Site Generator for WordPress
+ */
+
+namespace Ferparmur\WpStaticSiteGenerator;
+
+//Load Composer
+require __DIR__ . '/vendor/autoload.php';
+
+$plugin = new Plugin();
